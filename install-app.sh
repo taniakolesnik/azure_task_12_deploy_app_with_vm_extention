49e867a0a98afc9e345b0400d74e1f5779b880b1
Tania Kolesnik <kolesnik@gmail.com>
2026-01-22 23:03:52 +0000

    deploy install-app.sh

--- install-app.sh
+++ install-app.sh
@@ -6,18 +6,18 @@
 
 # install system updates and isntall python3-pip package using apt. '-yq' flags are 
 # used to suppress any interactive prompts - we won't be able to confirm operation 
-# when running the script as VM extention.  
-apt-get update -yq
-apt-get install python3-pip -yq
+# when running the script as VM extention.
+sudo apt-get update -yq
+sudo apt-get install python3-pip -yq
 
 # Create a directory for the app and download the files. 
-mkdir /app 
+sudo mkdir /app
 # make sure to uncomment the line bellow and update the link with your GitHub username
 # git clone https://github.com/<your-gh-username>/azure_task_12_deploy_app_with_vm_extention.git
-cp -r azure_task_12_deploy_app_with_vm_extention/app/* /app
+sudo cp -r azure_task_12_deploy_app_with_vm_extention/app/* /app
 
 # create a service for the app via systemctl and start the app
-mv /app/todoapp.service /etc/systemd/system/
-systemctl daemon-reload
-systemctl start todoapp
-systemctl enable todoapp
+sudo mv /app/todoapp.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start todoapp
+sudo systemctl enable todoapp

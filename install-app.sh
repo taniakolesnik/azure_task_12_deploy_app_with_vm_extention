7fa3f79becea94d40a842320706dc9fed188e016
Tania Kolesnik <kolesnik@gmail.com>
2026-01-22 23:18:12 +0000

    deploy install-app.sh

--- install-app.sh
+++ install-app.sh
@@ -3,7 +3,7 @@
 # Script to silently install and start the todo web app on the virtual machine. 
 # Note that all commands bellow are without sudo - that's because extention mechanism 
 # runs scripts under root user.
-sudo git clone https://github.com/taniakolesnik/azure_task_12_deploy_app_with_vm_extention.git
+
 
 # install system updates and isntall python3-pip package using apt. '-yq' flags are
 # used to suppress any interactive prompts - we won't be able to confirm operation 
@@ -14,7 +14,8 @@ sudo apt-get install python3-pip -yq
 # Create a directory for the app and download the files. 
 sudo mkdir /app
 # make sure to uncomment the line bellow and update the link with your GitHub username
-# git clone https://github.com/<your-gh-username>/azure_task_12_deploy_app_with_vm_extention.git
+echo "git clone https://github.com/taniakolesnik/azure_task_12_deploy_app_with_vm_extention.git"
+sudo git clone https://github.com/taniakolesnik/azure_task_12_deploy_app_with_vm_extention.git
 sudo cp -r azure_task_12_deploy_app_with_vm_extention/app/* /app
 
 # create a service for the app via systemctl and start the app
